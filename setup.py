6efaa82400af5b51ed5a4943e5853b5f4c9d8ff8
k <wulove5@gmail.com>
2018-06-09 23:53:45 +0800

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import io
+
+from setuptools import find_packages, setup
+
+with io.open('README.rst', 'rt', encoding='utf8') as f:
+    readme = f.read()
+
+setup(
+    name='KBS',
+    version='0.0.1',
+    # url='',
+    license='MIT',
+    maintainer='kms19',
+    maintainer_email='wulove5@gmail.com',
+    description='A basic KMS app',
+    long_description=readme,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
