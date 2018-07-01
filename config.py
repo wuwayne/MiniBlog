714a5f7fa368d8f84555cf30622c3f4bd09c767c
wuwayne <wulove5@gmail.com>
2018-07-01 17:21:32 +0800

    change TLS to SSL

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
 
 	MAIL_SERVER = os.environ.get('MAIL_SERVER')
 	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') is not None
 	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['wulove@gmail.com']
